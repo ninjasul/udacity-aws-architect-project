1d18b8e114c1b675dd8b61f048864998b467c7d1
박동엽 <ninjasul@onestorecorp.com>
2020-12-24 16:57:51 +0900

    commited image files

--- project2-design-provision-and-monitor-aws-infra-at-scale/task5-use-terraform-to-provision-aws-infra/Exercise_2/lambda.py
+++
@@ -1,4 +0,0 @@
-import os
-
-def greeting(event, context):
-    return "{} from Lambda!".format(os.environ['greeting'])
--- project2-design-provision-and-monitor-aws-infra-at-scale/task5-use-terraform-to-provision-aws-infra/Exercise_2/outputs.tf
+++
@@ -1,4 +0,0 @@
-# TODO: Define the output variable for the lambda function.
-output "lambda_greeting_id" {
-  value = aws_lambda_function.greeting.id
-}
